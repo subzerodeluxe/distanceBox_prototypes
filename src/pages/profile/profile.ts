0aa83fb4c36aea65db0e2c6620cf13195934d801
subzerodeluxe <maarten.waanders@gmail.com>
2017-03-19 22:03:21 +0100

    formatted date/time

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -16,13 +16,16 @@ export class ProfilePage {
   
   // properties
   blackJackTips = BlackJackTipsPage;
-  correctTime: string = "";
+  correctTime: any;
+  formattedDate: any;
+  correctDate: any; 
+  formattedTime: any; 
   nightTime: boolean = false; 
 
   constructor(public navCtrl: NavController, 
   private auth: AuthProvider, private timeZone: Timezone, public navParams: NavParams) {}
 
-
+ 
   getTime(city) {
     if (city == "tilburg") {
       var cityCode = "Europe/Amsterdam";
@@ -33,14 +36,47 @@ export class ProfilePage {
     var correctTime = this.timeZone.getTimeByCity(cityCode)
     .subscribe(resp => {
       console.log(resp); 
-      if (resp.countryCode == "ID") {
-        this.nightTime = true; 
-      } else {
-        this.nightTime = false; 
-      }
-      this.correctTime = resp.formatted; 
+      //this.correctTime = this.convertTime(resp.timestamp); 
+      this.correctTime = this.checkNight(resp.formatted);
+      this.correctDate = resp.formatted.substr(0, 10); 
+
     })
     
   }
+
+  checkNight(time) {
+    var correctTime = time.substr(11, 18);
+    var hours = Number(correctTime.substr(0, 2)); 
+    console.log("abu " + hours); 
+    
+    if ((hours >= 0 && hours <= 5) || (hours >= 18 && hours <= 23)) {
+        this.nightTime = true; 
+      } else {
+        this.nightTime = false; 
+      } 
+      return correctTime; 
+  }
+  
+
+  /*convertTime(timestamp) {
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+     if ((hours >= 0 && hours <= 5) || (hours >= 18 && hours <= 23)) {
+        this.nightTime = true; 
+      } else {
+        this.nightTime = false; 
+      } 
+
+    // Will display time in 10:30:23 format
+    return this.formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  }*/ 
 
 }
